a74cd96669616b1274a8112903c1801bdd412b22
Yin-Chi Chan <ycc39@cam.ac.uk>
2025-02-21 09:21:18 +0000

    Add code_style.sh

---
+++ code_style.sh
@@ -0,0 +1,12 @@
+#! /usr/bin/env bash
+# Sort imports and run pylint, recurse on entire src/ directory.
+# Assumes the Python project uses the `uv` package manager and
+# ensures `isort` and `pylint` are available in the virtual environment. 
+#
+# Copy this script to some directory on your $PATH, e.g. $HOME/bin
+
+GIT_ROOT=$(git rev-parse --show-toplevel)
+
+uv add --dev isort pylint autopep8
+uv run isort $GIT_ROOT/src/
+uv run pylint $GIT_ROOT/src/
